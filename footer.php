99ff2fc0033902198f3e7f775a4087b03b39d0a1
Matheo TRIKI <matheo.triki@gmail.com>
2024-04-10 17:39:14 +0200

    Modification footer

--- footer.php
+++ footer.php
@@ -1,17 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-<body>
-
-<div class="container">    
     <footer class="site-footer">
         <p>
             <strong>© THE ARTBOX</strong> - <em>Tous droits réservés</em>
         </p>
     </footer>
-</div>
 
-</body>
-</html>
